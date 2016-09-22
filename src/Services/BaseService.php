3a9a8eee5d72553c049f7698732175be218a3019
Arif Islam <arif@dreamfactory.com>
2016-09-22 15:36:17 +0000

    adding swagger definition

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -5,6 +5,7 @@ use DreamFactory\Core\Exceptions\BadRequestException;
 use DreamFactory\Core\Services\BaseRestService;
 use DreamFactory\Core\Exceptions\InternalServerErrorException;
 use DreamFactory\Core\Utility\Session;
+use DreamFactory\Library\Utility\Inflector;
 use Psr\Log\LoggerInterface;
 use Config;
 
@@ -71,6 +72,24 @@ abstract class BaseService extends BaseRestService
         return ['success' => $result];
     }
 
+    /** {@inheritdoc} */
+    protected function handleGET()
+    {
+        return false;
+    }
+
+    /** {@inheritdoc} */
+    protected function handlePATCH()
+    {
+        return false;
+    }
+
+    /** {@inheritdoc} */
+    protected function handleDELETE()
+    {
+        return false;
+    }
+
     /**
      * @return array
      */
@@ -99,5 +118,269 @@ abstract class BaseService extends BaseRestService
             'config'  => Config::get('df'),
             'session' => Session::all(),
         ];
+    }
+
+    /** {@inheritdoc} */
+    public static function getApiDocInfo($service)
+    {
+        $base = parent::getApiDocInfo($service);
+        $name = strtolower($service->name);
+        $capitalized = Inflector::camelize($service->name);
+
+        $base['paths'] = [
+            '/' . $name                 => [
+                'post' => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'parameters'        => [
+                        [
+                            'name'        => 'body',
+                            'description' => 'Content - Log level and message.',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'level'   => [
+                                        'type'        => 'string',
+                                        'description' => 'Valid levels: emergency, alert, critical, error, warning, notice, info, debug'
+                                    ],
+                                    'message' => [
+                                        'type'        => 'string',
+                                        'description' => 'Your log message goes here'
+                                    ]
+                                ]
+                            ],
+                            'in'          => 'body',
+                        ]
+                    ],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ],
+                'put'  => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'parameters'        => [
+                        [
+                            'name'        => 'body',
+                            'description' => 'Content - Log level and message.',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'level'   => [
+                                        'type'        => 'string',
+                                        'description' => 'Valid levels: emergency, alert, critical, error, warning, notice, info, debug'
+                                    ],
+                                    'message' => [
+                                        'type'        => 'string',
+                                        'description' => 'Your log message goes here'
+                                    ]
+                                ]
+                            ],
+                            'in'          => 'body',
+                        ]
+                    ],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ]
+            ],
+            '/' . $name . '/{urlencoded_message}' => [
+                'parameters' => [
+                    [
+                        'name'        => 'urlencoded_message',
+                        'description' => 'URL encoded log message.',
+                        'type'        => 'string',
+                        'in'          => 'path',
+                        'required'    => true,
+                    ],
+                ],
+                'post' => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ],
+                'put'  => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ]
+            ],
+            '/' . $name . '/{level}/{urlencoded_message}' => [
+                'parameters' => [
+                    [
+                        'name'        => 'level',
+                        'description' => 'Valid levels: emergency, alert, critical, error, warning, notice, info, debug',
+                        'type'        => 'string',
+                        'in'          => 'path',
+                        'required'    => true,
+                    ],
+                    [
+                        'name'        => 'urlencoded_message',
+                        'description' => 'URL encoded log message.',
+                        'type'        => 'string',
+                        'in'          => 'path',
+                        'required'    => true,
+                    ]
+                ],
+                'post' => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ],
+                'put'  => [
+                    'tags'              => [$name],
+                    'summary'           => 'create' .
+                        $capitalized .
+                        'Entries() - Create one log entry',
+                    'operationId'       => 'create' . $capitalized . 'Entries',
+                    'x-publishedEvents' => [
+                        $name . '.create'
+                    ],
+                    'consumes'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'produces'          => ['application/json', 'application/xml', 'text/csv', 'text/plain'],
+                    'responses'         => [
+                        '201'     => [
+                            'description' => 'Success',
+                            'schema'      => [
+                                'type'       => 'object',
+                                'properties' => [
+                                    'success' => [
+                                        'type' => 'boolean'
+                                    ]
+                                ]
+                            ]
+                        ],
+                        'default' => [
+                            'description' => 'Error',
+                            'schema'      => ['$ref' => '#/definitions/Error']
+                        ]
+                    ],
+                    'description'       => 'Creates one log entry.'
+                ]
+            ],
+        ];
+
+        return $base;
     }
 }
