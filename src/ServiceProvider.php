979607e505fba9601c264d2c4585ee44816f1efb
Lee Hicks <leehicks@dreamfactory.com>
2017-08-10 14:42:46 -0400

    Reworking API doc usage and generation.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 namespace DreamFactory\Core\Logger;
 
-use DreamFactory\Core\Components\ServiceDocBuilder;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\Logger\Handlers\Events\LoggingEventHandler;
 use DreamFactory\Core\Logger\Services\Logstash;
@@ -12,8 +11,6 @@ use Event;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    use ServiceDocBuilder;
-
     public function register()
     {
         // Add our service types.
@@ -25,9 +22,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                     'description'     => 'Logstash service.',
                     'group'           => ServiceTypeGroups::LOG,
                     'config_handler'  => LogstashConfig::class,
-                    'default_api_doc' => function ($service){
-                        return $this->buildServiceDoc($service->id, Logstash::getApiDocInfo($service));
-                    },
                     'factory'         => function ($config){
                         return new Logstash($config);
                     },
